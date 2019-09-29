db439ddd63ecb4bf924501abb8d504369dbda2df
Darpee <captain.darpee@gmail.com>
2019-09-29 14:07:26 +0200

    fichier de config+varaiables

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 const { Client } = require("discord.js");
-const client = new Client({disableEveryone: true });
+const { TOKEN, PREFIX } = require("./config");
+const client = new Client({ disableEveryone: true });
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log("je suis prêt!");
 });
 
 client.on("message", msg => {
-  if (msg.content === "ping") msg.reply("Pong!");
-  if (msg.content === "everyone") 
-   msg.channel.send("@everyone, salut à tous !", { disableEveryone: false });
-  if (msg.content === "noteveryone") 
-   msg.channel.send ("@everyone (noteveryone), salut à tous!");
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!");
+  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("Ping!");
 });
 
-client.login("NjI1MzExOTA1NTgxMzY3Mjk2.XZB9hw.qqBy7F3cR5M-_L6TLFJuPsD8xtw");
+client.login(TOKEN);
